24c7b9f68b9a0fd644045e7d1b37c7352b71ca89
buguen <bernard.uguen@univ-rennes1.fr>
2012-09-19 10:43:10 +0200

    project.py pep8

--- pylayers/util/project.py
+++ pylayers/util/project.py
@@ -2,171 +2,171 @@
 import os
 #class Project(object)
 #       """
-#       Création d'une arborescence de projet 
+#       Création d'une arborescence de projet
 #       """
 #       def __init__(self):
-#       def     
+#       def
 currentdir = os.getcwd()
-try: 
+try:
     pulsraydir = os.environ['PULSRAY']
-    print "PULSRAY  : ",pulsraydir
+    print "PULSRAY  : ", pulsraydir
 except:
-    raise EnvironmentError('Please set the PULSRAY environment variable') 
+    raise EnvironmentError('Please set the PULSRAY environment variable')
 try:
     figuredir = os.environ['FIGURDIR']
 except:
-    figuredir = os.environ['HOME']+"/Pyproject/figures"
+    figuredir = os.environ['HOME'] + "/Pyproject/figures"
 #print currentdir
-print "FIGURDIR : ",figuredir
+print "FIGURDIR : ", figuredir
 try:
-        basename=os.environ['BASENAME']
+    basename = os.environ['BASENAME']
 except:
-        basename=os.environ['HOME']+"/Pyproject"
+    basename = os.environ['HOME'] + "/Pyproject"
 
-print "BASENAME : ",basename
+print "BASENAME : ", basename
 try:
-        os.chdir(basename)
-        strdir = basename + '/struc' 
-        try:
-                os.chdir(strdir)
-                #print "struc dir already exist"
-                os.chdir('..')
-        except: 
-                print "create struc dir"
-                os.mkdir(strdir)
-                os.chdir('..')
+    os.chdir(basename)
+    strdir = basename + '/struc'
+    try:
+        os.chdir(strdir)
+        #print "struc dir already exist"
+        os.chdir('..')
+    except:
+        print "create struc dir"
+        os.mkdir(strdir)
+        os.chdir('..')
 
-        geomdir = basename + '/geom' 
-        try:
-                os.chdir(geomdir)
-                #print "geom dir already exist"
-                os.chdir('..')
-        except: 
-                os.mkdir(geomdir)
-                print "create geom dir"
-                os.chdir('..')
-        
-        lchdir = basename + '/launch' 
-        try:
-                os.chdir(lchdir)
-                #print "launch dir already exist"
-                os.chdir('..')
-        except: 
-                print "create launch dir"
-                os.mkdir(lchdir)
-                os.chdir('..')
-        
-        tuddir = basename + '/tud' 
-        try:
-                os.chdir(tuddir)
-                #print "tud dir already exist"
-                os.chdir('..')
-        except: 
-                print "create tud dir"
-                os.mkdir(tuddir)
-                os.chdir('..')
+    geomdir = basename + '/geom'
+    try:
+        os.chdir(geomdir)
+        #print "geom dir already exist"
+        os.chdir('..')
+    except:
+        os.mkdir(geomdir)
+        print "create geom dir"
+        os.chdir('..')
 
-        slabdir = basename + '/slab' 
-        try:
-                os.chdir(slabdir)
-                #print "slab dir already exist"
-                os.chdir('..')
-        except: 
-                print "create slab dir"
-                os.mkdir(slabdir)
-                os.chdir('..')
+    lchdir = basename + '/launch'
+    try:
+        os.chdir(lchdir)
+        #print "launch dir already exist"
+        os.chdir('..')
+    except:
+        print "create launch dir"
+        os.mkdir(lchdir)
+        os.chdir('..')
 
-        matdir = basename + '/mat' 
-        try:
-                os.chdir(matdir)
-                #print "mat dir already exist"
-                os.chdir('..')
-        except: 
-                print "create mat dir"
-                os.mkdir(matdir)
-                os.chdir('..')
-        
-        tradir = basename + '/trace' 
-        try:
-                os.chdir(geomdir)
-                #print "trace dir already exist"
-                os.chdir('..')
-        except: 
-                print "create trace dir"
-                os.mkdir(tradir)
-                os.chdir('..')
+    tuddir = basename + '/tud'
+    try:
+        os.chdir(tuddir)
+        #print "tud dir already exist"
+        os.chdir('..')
+    except:
+        print "create tud dir"
+        os.mkdir(tuddir)
+        os.chdir('..')
 
-        geomdir = basename + 'geom' 
-        try:
-                os.chdir(geomdir)
-                #print "geom dir already exist"
-                os.chdir('..')
-        except: 
-                print "create geom dir"
-                os.mkdir(geomdir)
-                os.chdir('..')
-        
-        antdir = basename + '/ant' 
-        try:
-                os.chdir(antdir)
-                #print "ant dir already exist"
-                os.chdir('..')
-        except: 
-                print "create ant dir"
-                os.mkdir(antdir)
-                os.chdir('..')
+    slabdir = basename + '/slab'
+    try:
+        os.chdir(slabdir)
+        #print "slab dir already exist"
+        os.chdir('..')
+    except:
+        print "create slab dir"
+        os.mkdir(slabdir)
+        os.chdir('..')
 
- 	tuddir = basename + '/tud' 
-        try:
-                os.chdir(tuddir)
-                #print "tud dir already exist"
-                os.chdir('..')
-        except: 
-                print "create tud dir"
-                os.mkdir(tuddir)
-                os.chdir('..')
+    matdir = basename + '/mat'
+    try:
+        os.chdir(matdir)
+        #print "mat dir already exist"
+        os.chdir('..')
+    except:
+        print "create mat dir"
+        os.mkdir(matdir)
+        os.chdir('..')
 
-	mesdir = basename + '/measures' 
-        try:
-                os.chdir(mesdir)
-                #print "tud dir already exist"
-                os.chdir('..')
-        except: 
-                print "create mes dir"
-                os.mkdir(mesdir)
-                os.chdir('..')
-	simuldir = basename + '/simul' 
-        try:
-                os.chdir(simuldir)
-                #print "tud dir already exist"
-                os.chdir('..')
-        except: 
-                print "create simul dir"
-                os.mkdir(simuldir)
-                os.chdir('..')
-	wavedir = basename + '/wave' 
-        try:
-                os.chdir(wavedir)
-                #print "wavedir already exist"
-                os.chdir('..')
-        except: 
-                print "create wavedir"
-                os.mkdir(wavedir)
-                os.chdir('..')
-	cirdir = basename + '/cir' 
-        try:
-                os.chdir(cirdir)
-                #print "wavedir already exist"
-                os.chdir('..')
-        except: 
-                print "create cirdir"
-                os.mkdir(cirdir)
-                os.chdir('..')
+    tradir = basename + '/trace'
+    try:
+        os.chdir(geomdir)
+        #print "trace dir already exist"
+        os.chdir('..')
+    except:
+        print "create trace dir"
+        os.mkdir(tradir)
+        os.chdir('..')
 
-       
+    geomdir = basename + 'geom'
+    try:
+        os.chdir(geomdir)
+        #print "geom dir already exist"
+        os.chdir('..')
+    except:
+        print "create geom dir"
+        os.mkdir(geomdir)
+        os.chdir('..')
+
+    antdir = basename + '/ant'
+    try:
+        os.chdir(antdir)
+        #print "ant dir already exist"
+        os.chdir('..')
+    except:
+        print "create ant dir"
+        os.mkdir(antdir)
+        os.chdir('..')
+
+    tuddir = basename + '/tud'
+    try:
+        os.chdir(tuddir)
+        #print "tud dir already exist"
+        os.chdir('..')
+    except:
+        print "create tud dir"
+        os.mkdir(tuddir)
+        os.chdir('..')
+
+    mesdir = basename + '/measures'
+    try:
+        os.chdir(mesdir)
+        #print "tud dir already exist"
+        os.chdir('..')
+    except:
+        print "create mes dir"
+        os.mkdir(mesdir)
+        os.chdir('..')
+    simuldir = basename + '/simul'
+    try:
+        os.chdir(simuldir)
+        #print "tud dir already exist"
+        os.chdir('..')
+    except:
+        print "create simul dir"
+        os.mkdir(simuldir)
+        os.chdir('..')
+    wavedir = basename + '/wave'
+    try:
+        os.chdir(wavedir)
+        #print "wavedir already exist"
+        os.chdir('..')
+    except:
+        print "create wavedir"
+        os.mkdir(wavedir)
+        os.chdir('..')
+    cirdir = basename + '/cir'
+    try:
+        os.chdir(cirdir)
+        #print "wavedir already exist"
+        os.chdir('..')
+    except:
+        print "create cirdir"
+        os.mkdir(cirdir)
+        os.chdir('..')
+
+
 except:
-        print "directory " + basename + " does'n exist"
-        print "Create directory "
-        os.mkdir(basename)
+    print "directory " + basename + " does'n exist"
+    print "Create directory "
+    os.mkdir(basename)
 
 os.chdir(currentdir)
