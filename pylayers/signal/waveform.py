b1feec26e1b0aa703ae124efbc30d62122c3ac3b
buguen <bernard.uguen@univ-rennes1.fr>
2013-04-18 12:58:51 +0200

    DEV-RayTracing.ipynb

--- pylayers/signal/waveform.py
+++ pylayers/signal/waveform.py
@@ -1,6 +1,7 @@
 # -*- coding:Utf-8 -*-
 import doctest
 import os
+import pdb
 import numpy as np
 import scipy as sp
 import scipy.io as io
@@ -33,11 +34,10 @@ class Waveform:
             self.parameters = param
         else:
             self.parameters = parameters
-        
         self.eval()
 
     def eval(self):
-        """ evaluate waveform 
+        """ evaluate waveform
 
             The lambda/4*pi factor which is necessary to get the proper budget 
             link ( from the fris formula) is introduced in this function.
@@ -64,6 +64,8 @@ class Waveform:
 
         Results
         -------
+
+        >>> from pylayers.signal.waveform import *
         >>> param= {'type' : 'generic',\
             'band': 0.499,\
             'fc': 4.493,\
@@ -123,36 +125,50 @@ class Waveform:
 
     def fromfile(self):
         """
-        get the measurement waveform
+        get the measurement waveform from WHERE1 measurement campaign
+
+        This function is not yet generic
+
+        >>> from pylayers.signal.waveform import *
+        >>> wav = Waveform({'type':'file'})
+        >>> wav.show()
+
         """
-        Tw     = self.parameters['tw']
-        fc     = self.parameters['fc']
-        band   = self.parameters['band']
-        thresh = self.parameters['thresh']
-        fe     = self.parameters['fe']
-        te     = 1.0/fe
-        self.parameters['te'] = te
-        Np     = fe*Tw
-        self.parameters['Np']=Np
-    
         M = mesuwb.UWBMesure(1,1)
-        w   = bs.TUsignal()
-        timeTx = M.RAW_DATA.timetx[0]*1e9
-        timeI = timeTx[1]-timeTx[0]
-        Tx = M.RAW_DATA.tx[0]
-        u=np.where(Tx==max(Tx))[0][0]
-    
-        Tx3=Tx[u:]
-        Tx2=Tx[0:u]
-        Tx1=np.zeros(len(Tx3)-len(Tx2))
-        
-        t=np.arange(0,timeTx[-1]-timeTx[u]+0.5*timeI,timeI)
-        tm=np.arange(-(timeTx[-1]-timeTx[u]),0,timeI)
-    
-        rTx=np.hstack((Tx1,np.hstack((Tx2,Tx3))))
-        rt=np.hstack((tm,t))
-        w.x=rt
-        w.y=rTx*(1/np.sqrt(30))
+        w = bs.TUsignal()
+
+        ts = M.RAW_DATA.timetx[0]
+        tns = ts*1e9
+        te = tns[1]-tns[0]
+
+        y  = M.RAW_DATA.tx[0]
+
+        # find peak position  u is the index of the peak
+        # yap :after peak
+        # ybp : before peak
+        # yzp : zero padding
+        maxy = max(y)
+        u = np.where(y ==maxy)[0][0]
+        yap = y[u:]
+        ybp = y[0:u]
+
+        yzp = np.zeros(len(yap)-len(ybp)-1)
+
+        tnsp = np.arange(0,tns[-1]-tns[u]+0.5*te,te)
+        tnsm = np.arange(-(tns[-1]-tns[u]),0,te)
+
+        y = np.hstack((yzp,np.hstack((ybp,yap))))
+        tns = np.hstack((tnsm,tnsp))
+
+        #
+        # Warning (check if 1/sqrt(30) is not applied elsewhere
+        #
+        w.x = tns
+        w.y = y*(1/np.sqrt(30))
+
+        #  w : TUsignal
+        #  W : FUsignal (Hermitian redundancy removed)
+
         W   = w.ftshift()
         return (w,W)
 
@@ -221,22 +237,28 @@ class Waveform:
             st.plot()
             show()
 
-    def show(self):
+    def show(self,fig=[]):
         """
         """
+        # title construction
+        if fig ==[]:
+            fig = plt.figure()
         title =''
-        for st in self.parameters.keys():
-            val   = self.parameters[st]
-            title = title + st + ': '
+        for pk in self.parameters.keys():
+            val   = self.parameters[pk]
+            title = title + pk + ': '
             if type(val) != 'str':
                 title = title + str(val) + ' '
-        fig = plt.figure()
-        plt.title(title)
-        fig.add_subplot(211)
-        self.st.plot()
-        fig.add_subplot(212)
-        plt.plot(self.sf.x,abs(self.sf.y))
-
+        #plt.title(title)
+        ax1 = fig.add_subplot(2,1,1)
+        ax1.plot(self.st.x,self.st.y)
+        plt.xlabel('time (ns)')
+        plt.ylabel('level in linear scale')
+        ax2 = fig.add_subplot(2,1,2)
+        ax2.plot(self.sf.x,abs(self.sf.y))
+        plt.xlabel('frequency (GHz)')
+        plt.ylabel('level in linear scale')
+        fig.suptitle(title)
 
 
 if __name__ == "__main__":
