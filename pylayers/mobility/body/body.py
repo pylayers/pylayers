6cf1153d1d1496cbb771f663d6dfe97c86243d42
Meriem Mhedhbi <meriem.mhedhbi@univ-rennes1.fr>
2013-07-03 12:08:38 +0200

    commenting examples in body

--- pylayers/mobility/body/body.py
+++ pylayers/mobility/body/body.py
@@ -1,11 +1,13 @@
 import numpy as np
-import scipy.stats as sp
+g = plt.figure()                                                                                     
+                                                                                import scipy.stats as sp
 from pylayers.mobility.body import c3d
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import networkx as nx
 import  pdb as pdb
 import pylayers.util.plotutil as pltu
+import doctest
 
 
 def Basis_generation(A, B):
@@ -207,6 +209,24 @@ class Body(object):
 
 
 def translate(cycle, new_origin):
+    """ rotate a cycle of frames by an angle alpha
+
+    Parameters
+    ----------
+
+    cycle :  np.array
+            3 x np x nf
+    alpha : float
+        angle in radians
+
+
+    Returns
+    -------
+
+    cycle modified : np.array
+        3 x np x nf
+    """
+
 
     cycle_tr = np.ndarray(shape=cycle.shape)
     old_origin = cycle[:, 0, 0]
@@ -216,25 +236,51 @@ def translate(cycle, new_origin):
     return cycle_tr
 
 
-def rotation(cycle, alpha):
+def rotation(cycle, alpha=np.pi/2):
+    """ rotate a cycle of frames by an angle alpha
+
+    Parameters
+    ----------
+
+    cycle :  np.array
+            3 x np x nf
+    alpha : float
+        angle in radians
+
+
+    Returns
+    -------
+
+    cycle modified : np.array
+        3 x np x nf
+    """
+
 
     cycle_rot = np.ndarray(shape=cycle.shape)
-    cycle_rot[0, :, :] = (
-        cycle[0, :, :]) * np.cos(alpha) + (cycle[1, :, :]) * np.sin(alpha)
-    cycle_rot[1, :, :] = -(cycle[0, :, :]) * np.sin(alpha) + (cycle[1,
-                                                                    :, :]) * np.cos(alpha)
+    cycle_rot[0, :, :] = (cycle[0, :, :]) * np.cos(alpha) + (cycle[1, :, :]) * np.sin(alpha)
+    cycle_rot[1, :, :] = -(cycle[0, :, :]) * np.sin(alpha) + (cycle[1, :, :]) * np.cos(alpha)
     cycle_rot[2, :, :] = cycle[2, :, :]
     cycle_rot = translate(cycle_rot, cycle[:, 0, 0])
+
     return cycle_rot
 
 
 def Global_Trajectory(cycle, traj):
-
     """
+
+    Parameters
+    ----------
+
     cycle :  walking step cycle (2 step), shape = (3,npoints  = 15, nframes = 126)
     traj  : trajectory described by the gravity center, shape =(3,nposition)
+
     We assume that the body moves straight between two successive positions
 
+    Returns
+    -------
+
+    data : list
+        list
     """
 
     data = []
@@ -247,6 +293,7 @@ def Global_Trajectory(cycle, traj):
     v = v - np.dot(np.dot(v, vect_ortho), vect_ortho)
     v[2] = 0
     vect_ant = v / np.linalg.norm(v)
+
     for i in range(1, traj.shape[1]):
 
         print 'i = ', i
@@ -255,6 +302,7 @@ def Global_Trajectory(cycle, traj):
         vect_depl = vect_depl / np.linalg.norm(vect_depl)
 
         alpha = np.arccos(np.dot(vect_ant, vect_depl))
+
         cycle_i = rotation(cycle, alpha)
 
         dist_inter = dist(traj.T[i], traj.T[i - 1])
@@ -274,138 +322,6 @@ def Global_Trajectory(cycle, traj):
 
 
 if __name__ == '__main__':
+    plt.ion()
+    doctest.testmod()
 
-    nframes = 126
-    B = Body()
-    plt.figure()
-    nx.draw(B.g)
-    plt.axis('scaled')
-
-    ############# verification de la base
-    #B.LoadMotion()
-    #c3d_frames = B.d
-    #B.CylinderModel()
-    #fr_id  = 125
-    #B.cylinder_basis0()
-    #B.cylinder_basis_k (frameId = fr_id)
-    #P = np.array([1,1,1])
-    #for cy_id in range(10):
-    #A = np.zeros(shape = (3))
-    #u0 = B.basis0[cy_id,0:3]
-    #v0 = B.basis0[cy_id,3:6]
-    #w0 = B.basis0[cy_id,6:]
-    #fig  = plt.figure()
-    #ax = fig.add_subplot(111, projection='3d')
-    #SF = 1
-    #plt.plot((A[0],SF*u0[0]+A[0]),(A[1],SF*u0[1]+A[1]),(A[2],SF*u0[2]+A[2]), 'y')
-    #plt.plot((A[0],SF*v0[0]+A[0]),(A[1],SF*v0[1]+A[1]),(A[2],SF*v0[2]+A[2]),'r')
-    #plt.plot((A[0],SF*w0[0]+A[0]),(A[1],SF*w0[1]+A[1]),(A[2],SF*w0[2]+A[2]),'g')
-    #uk = B.basisk[cy_id,0:3]
-    #vk = B.basisk[cy_id,3:6]
-    #wk = B.basisk[cy_id,6:]
-    #plt.plot((A[0],SF*uk[0]+A[0]),(A[1],SF*uk[1]+A[1]),(A[2],SF*uk[2]+A[2]), 'y--')
-    #plt.plot((A[0],SF*vk[0]+A[0]),(A[1],SF*vk[1]+A[1]),(A[2],SF*vk[2]+A[2]),'r--')
-    #plt.plot((A[0],SF*wk[0]+A[0]),(A[1],SF*wk[1]+A[1]),(A[2],SF*wk[2]+A[2]),'g--')
-    #plt.autoscale()
-    #plt.title(str(cy_id))
-    #plt.show()
-    ############   Antenna on cylinders
-    B.LoadMotion()
-    c3d_frames = B.d
-    B.CylinderModel()
-    B.cylinder_basis0()
-
-    fr_ind = 0
-    cyl = B.c[:, :, fr_ind]
-    SF = 1
-    l = 10
-    alpha = np.pi / 3
-
-    cy_id = range(0, 10)
-    l = np.ones((10))
-    alpha = np.pi / 2 * np.ones((10))
-    B.cyl_antenna(cy_id, l, alpha, 0)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect='equal', projection='3d')
-    for i in range(B.c.shape[0]):
-        pltu.cylinder(fig, cyl[i, 1:4], cyl[i, 4:7], cyl[i, 7])
-        A3 = cyl[i, 1:4]
-        u3 = B.basis0[i, 0:3]
-        v3 = B.basis0[i, 3:6]
-        w3 = B.basis0[i, 6:]
-
-        plt.plot((A3[0], SF * u3[0] + A3[0]), (
-            A3[1], SF * u3[1] + A3[1]), (A3[2], SF * u3[2] + A3[2]), 'y--')
-        plt.plot((A3[0], SF * v3[0] + A3[0]), (
-            A3[1], SF * v3[1] + A3[1]), (A3[2], SF * v3[2] + A3[2]), 'r--')
-        plt.plot((A3[0], SF * w3[0] + A3[0]), (
-            A3[1], SF * w3[1] + A3[1]), (A3[2], SF * w3[2] + A3[2]), 'g--')
-        plt.plot((A3[0], B.ant[i, 0] + A3[0]), (
-            A3[1], B.ant[i, 1] + A3[1]), (A3[2], B.ant[i, 2] + A3[2]), 'c*--')
-        #~ plt.axis('tight')
-        #~ fig  = plt.figure()
-        #~ plt.plot((A3[0],SF*u3[0]+A3[0]),(A3[1],SF*u3[1]+A3[1]), 'y--')
-        #~ plt.plot((A3[0],SF*v3[0]+A3[0]),(A3[1],SF*v3[1]+A3[1]),'r--')
-        #~ plt.plot((A3[0],SF*w3[0]+A3[0]),(A3[1],SF*w3[1]+A3[1]),'g--')
-            #~
-        #~ plt.plot((A3[0],B.ant[0]+ A3[0]) ,(A3[1],B.ant[1]+ A3[1]),'c*--')
-        #~ plt.axis('scaled')
-
-    fr_ind2 = 60
-    cyl1 = B.c[:, :, fr_ind2]
-    B.cylinder_basis_k(frameId=fr_ind2)
-    B.cyl_antenna(cy_id, l, alpha, fr_ind2)
-    SF = 1
-
-    fig = plt.figure()
-    for i in range(B.c.shape[0]):
-        pltu.cylinder(fig, cyl1[i, 1:4], cyl1[i, 4:7], cyl1[i, 7])
-        A3 = cyl1[i, 1:4]
-        u3 = B.basisk[i, 0:3]
-        v3 = B.basisk[i, 3:6]
-        w3 = B.basisk[i, 6:]
-
-        plt.plot((A3[0], SF * u3[0] + A3[0]), (
-            A3[1], SF * u3[1] + A3[1]), (A3[2], SF * u3[2] + A3[2]), 'y--')
-        plt.plot((A3[0], SF * v3[0] + A3[0]), (
-            A3[1], SF * v3[1] + A3[1]), (A3[2], SF * v3[2] + A3[2]), 'r--')
-        plt.plot((A3[0], SF * w3[0] + A3[0]), (
-            A3[1], SF * w3[1] + A3[1]), (A3[2], SF * w3[2] + A3[2]), 'g--')
-        plt.plot((A3[0], B.ant[i, 0] + A3[0]), (
-            A3[1], B.ant[i, 1] + A3[1]), (A3[2], B.ant[i, 2] + A3[2]), 'c*--')
-        #~ plt.axis('tight')
-
-    plt.show()
-
-    ####################
-    B.LoadMotion()
-    f = B.d
-    cycle_distance = dist(f[:, 0, 0], f[:, 0, -1])
-    print 'distance parcouru pendant un cycle de marche = ', cycle_distance
-    #~ for fr_id in range(0,124):
-        #~ inter_frames_dist = dist(f[:,0,fr_id],f[:,0,fr_id  +1])
-        #~ print ' frame ID = ' , fr_id ,  'distance parcouru pendant un step = ', inter_frames_dist
-#~
-    #~
-
-    ####### test translation and rotation
-    fig = plt.figure()
-
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(f[0, :, 0], f[1, :, 0], f[2, :, 0], c='g', marker='o')
-    ax.scatter(f[0, :, -1], f[1, :, -1], f[2, :, -1], c='g', marker='*')
-
-    # translation
-    #~ p = np.array([0,0,0])
-    #~ f1 = translate(f,p)
-    #~ ax.scatter(f1[0,:,0],f1[1,:,0],f1[2,:,0] )
-    #~ ax.scatter(f1[0,:,-1],f1[1,:,-1],f1[2,:,-1] )
-    # rotantion
-    #~ fig = plt.figure()
-#~
-    #~ ax = fig.add_subplot(111, projection='3d')
-    f2 = rotation(f, np.pi / 2)
-    ax.scatter(f2[0, :, 0], f2[1, :, 0], f2[2, :, 0], c='r', marker='o')
-    ax.scatter(f2[0, :, -1], f2[1, :, -1], f2[2, :, -1], c='r', marker='*')
-    plt.show()
