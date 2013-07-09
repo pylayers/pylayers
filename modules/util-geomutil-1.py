58b306952d7ea9e1a678ecde025eaf7108798f2d
uguen <uguen@uguen-Precision-M6700.(none)>
2013-07-09 19:18:42 +0200

    update documentation

--- modules/util-geomutil-1.py
+++ modules/util-geomutil-1.py
@@ -5,9 +5,6 @@ l1 = np.array([[0,1,1,0],[0,0,1,1]])
 L1 = LineString(l1)
 l2 = [[3,4,4,3],[1,1,2,2]]
 L2 = LineString(l2)
-l3 = [np.array([10,10]),np.array([11,10]),np.array([11,11]),np.array([10,11])]
-L3 = LineString(l3)
 fig,ax = L1.plot(color='red',alpha=0.3,linewidth=3)
 fig,ax = L2.plot(fig=fig,ax=ax,color='blue',alpha=0.7,linewidth=2)
-fig,ax = L3.plot(fig=fig,ax=ax,color='green',alpha=1,linewidth=1)
 title = plt.title('test plotting LineString')
--- modules/util-geomutil-6.py
+++ modules/util-geomutil-6.py
@@ -1,12 +1,26 @@
+import scipy as sp
+import numpy as np
 from pylayers.util.geomutil import *
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax  = fig.gca()
-p1 = np.array([0,0])
-p2 = np.array([1,0])
-p3 = np.array([0,1])
-p4 = np.array([1,1])
-ax = linet(ax,p1,p2,al=0.7,color='red',linewidth=3)
-ax = linet(ax,p2,p3,al=0.8,color='blue',linewidth=2)
-ax = linet(ax,p3,p4,al=0.9,color='green',linewidth=1)
-ax = linet(ax,p4,p1,al=1,color='cyan',linewidth=0.2)
+from pylayers.util.plotutil import *
+import matplotlib.pylab as plt
+N = 10
+A = sp.rand(2,N)
+B = sp.rand(2,N)
+C = sp.rand(2,N)
+D = sp.rand(2,N)
+b1 = intersect(A,B,C,D)
+pt1 = A[:,b1]
+ph1 = B[:,b1]
+pt2 = C[:,b1]
+ph2 = D[:,b1]
+f1,a1 = displot(pt1,ph1,'r')
+f2,a2 = displot(pt2,ph2,'b')
+ti = plt.title('test intersect')
+A = np.array([[0],[0]])
+B = np.array([[1],[1]])
+C = np.array([[1],[0]])
+D = np.array([[0],[1]])
+intersect(A,B,C,D)
+# array([ True], dtype=bool)
+intersect(A,B,C,D)[0]
+# True
