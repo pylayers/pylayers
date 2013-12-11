2e778e807d154d1fc15653a9a90b52a2db3dbf68
Nicolas AMIOT <amiot.nicolas@gmail.com>
2013-12-11 12:40:21 +0100

    add colored signature.info()

--- pylayers/util/pyutil.py
+++ pylayers/util/pyutil.py
@@ -1173,7 +1173,7 @@ def corrcy(a,b):
     return(tk)
 
 def foo(var1, var2, long_var_name='hi') :
-    r"""A one-line summary that does not use variable names or the
+    """A one-line summary that does not use variable names or the
     function name.
 
     Several sentences providing an extended description. Refer to
@@ -1622,6 +1622,33 @@ def fill_block_diag(A, blocks,M,k=0):
     for i in range(0,len(A)-abs(k)*M,M):
         A[i+ro:i+ro+M,i+co:i+co+M]=blocks[int(i/M),:,:] 
     return A
+
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#following from Python cookbook, #475186
+def has_colours(stream):
+    if not hasattr(stream, "isatty"):
+        return False
+    if not stream.isatty():
+        return False # auto color only on TTYs
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        # guess false in case of error
+        return False
+has_colours = has_colours(sys.stdout)
+
+
+def printout(text, colour=WHITE):
+        if has_colours:
+                seq = "\x1b[1;%dm" % (30+colour) + text + "\x1b[0m"
+                sys.stdout.write(seq)
+        else:
+                sys.stdout.write(text)
+
 
 if __name__ == "__main__":
     doctest.testmod()
