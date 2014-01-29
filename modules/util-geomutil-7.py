4866ba4c4fba9edef5de5e875ab517c289a12744
Bernard <bernard.uguen@univ-rennes1.fr>
2014-01-29 18:59:33 +0100

    updating web site channel is reciprocal

--- modules/util-geomutil-7.py
+++ modules/util-geomutil-7.py
@@ -1,12 +1,16 @@
+# >>>from pylayers.util.plotutil import *
+import scipy as sp
+import numpy as np
 from pylayers.util.geomutil import *
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax  = fig.gca()
-p1 = np.array([0,0])
-p2 = np.array([1,0])
-p3 = np.array([0,1])
-p4 = np.array([1,1])
-ax = linet(ax,p1,p2,al=0.7,color='red',linewidth=3)
-ax = linet(ax,p2,p3,al=0.8,color='blue',linewidth=2)
-ax = linet(ax,p3,p4,al=0.9,color='green',linewidth=1)
-ax = linet(ax,p4,p1,al=1,color='cyan',linewidth=0.2)
+from pylayers.util.plotutil import *
+import matplotlib.pylab as plot
+N = 20
+A = sp.rand(2,N)
+B = sp.rand(2,N)
+C = np.array(([0.5,0.5])).reshape(2,1)
+left=isleft(A,B,C)
+il = np.where(left)[0]
+inl = np.where(~left)[0]
+plt.scatter(C[0],C[1],color='b',s=10)
+displot(A[:,il],B[:,il],arrow=True,color='g')
+displot(A[:,inl],B[:,inl],arrow=True,color='r')
