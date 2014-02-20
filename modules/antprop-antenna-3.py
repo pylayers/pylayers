6751f34add126322511cf653cbcf38bb8846de74
Bernard <bernard.uguen@univ-rennes1.fr>
2014-02-20 21:49:00 +0100

    add antenna

--- modules/antprop-antenna-3.py
+++ modules/antprop-antenna-3.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
 from pylayers.antprop.antenna import *
 A = Antenna('defant.trx')
-fig,ax = A.polar(k=[0,10,50])
+fig,ax = A.polar(fGHz=[2,3,4],phd=0)
+fig,ax = A.polar(fGHz=[2,3,4],thd=90)
