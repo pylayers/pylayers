b6a5b8056b4e38bc281fcf399ef1eee2547c1cae
Bernard <bernard.uguen@univ-rennes1.fr>
2014-03-07 18:16:21 +0100

    Updating Website

--- modules/generated/pylayers-mobility-ban-body-Body-settopos-1.py
+++ modules/generated/pylayers-mobility-ban-body-Body-settopos-1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pylayers.mobility.trajectory as tr
-import pylayers.mobility.body.body as body
+import pylayers.mobility.ban.body as body
 import matplotlib.pyplot as plt
 time = np.arange(0,10,0.1)
 v = 4000/3600.
