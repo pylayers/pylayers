4a70d07f3980911213a7e8b1a1cf0fd687977c98
Bernard <bernard.uguen@univ-rennes1.fr>
2014-03-12 15:21:23 +0100

    Updating Website

--- modules/generated/pylayers-signal-bsignal-TUsignal-tau_rms-1.py
+++ modules/generated/pylayers-signal-bsignal-TUsignal-tau_rms-1.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 M = UWBMeasure(1)
 ch4 = M.tdd.ch4
 ch4.plot(color='k')
+plt.title("WHERE1 M1 UWB Channel impulse response")
 ch4.plot(color='k')
-plt.axis([10,160,-90,-50])
+plt.title("WHERE1 M1 UWB Channel impulse response")
+ax1=plt.axis([10,160,-90,-50])
 ch4.plot((color='k')
-plt.axis([20,120,-80,-50])
+plt.title("WHERE1 M1 UWB Channel impulse response")
+ax2=plt.axis([20,120,-80,-50])
 plt.show()
 tau_moy = ch4.tau_moy()
 print "tau_moy: %2.2f ns" % tau_moy
