603a4527ed4470e1ffe6f58ba4a4b777ab077ef0
Bernard <bernard.uguen@univ-rennes1.fr>
2014-03-12 15:29:01 +0100

    Updating Website

--- modules/generated/pylayers-signal-bsignal-TUsignal-tau_rms-1.py
+++ modules/generated/pylayers-signal-bsignal-TUsignal-tau_rms-1.py
@@ -10,7 +10,6 @@ ax1=plt.axis([10,160,-90,-50])
 ch4.plot(color='k')
 plt.title("WHERE1 M1 UWB Channel impulse response")
 ax2=plt.axis([20,120,-80,-50])
-plt.show()
 tau_moy = ch4.tau_moy()
 print "tau_moy: %2.2f ns" % tau_moy
 tau_rms = ch4.tau_rms()
