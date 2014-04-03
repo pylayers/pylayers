d41f9375588e47f3d4efab441096f78167b7cbaf
Bernard <bernard.uguen@univ-rennes1.fr>
2014-04-03 13:00:42 +0200

    Updating Website

--- modules/generated/pylayers-signal-bsignal-FUDsignal-minphas-1.py
+++ modules/generated/pylayers-signal-bsignal-FUDsignal-minphas-1.py
@@ -4,8 +4,10 @@ fGHz = np.arange(2,11,0.1)
 tau1 = np.array([1,2,3])[:,np.newaxis]
 y = np.exp(-2*1j*np.pi*fGHz[np.newaxis,:]*tau1)/fGHz[np.newaxis,:]
 H = FUDsignal(x=fGHz,y=y,taud=np.array([15,17,18]))
-f,a = H.plot(typ=['ru'])
+f,a = H.plot(typ=['ru'],xlabels=['Frequency GHz'])
+t1 = plt.title('Before minimal phase compensation')
 H.minphas()
 H.taue
 # array([ 1.,  2.,  3.])
 f,a = H.plot(typ=['ru'])
+t2 = plt.title('After minimal phase compensation')
