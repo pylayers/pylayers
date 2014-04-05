3ad9aa4ae8225720ca9c7336e2ca3c1b7fa977e1
Bernard <bernard.uguen@univ-rennes1.fr>
2014-04-05 16:32:43 +0200

    Updating Website

--- modules/generated/pylayers-signal-bsignal-FUDsignal-totime-1.py
+++ modules/generated/pylayers-signal-bsignal-FUDsignal-totime-1.py
@@ -1,19 +1,8 @@
-L = lk.DLink(verbose=False)
+L = DLink(verbose=False)
 aktk = L.eval()
-H = L.H
-
-f,a = H.show(cmap='jet')
-
-# We keep only the significant rays
-
-H.cut()
-f,a = H.show(cmap='jet')
-
-# We then transform into the time domain
-
-T1 = H.totime()
+L.H.cut()
+T1 = L.H.totime()
 T1.plot()
-
-H.minphas()
-T2 = H.totime()
+L.H.minphas()
+T2 = L.H.totime()
 T2.plot()
