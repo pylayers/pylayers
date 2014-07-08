d83a54fa04dece6d1586f52b93b4a2ac3ea28679
Nicolas AMIOT <amiot.nicolas@gmail.com>
2014-07-08 12:01:14 +0200

    work in read hikob

--- pylayers/measures/Hikobstart.py
+++ pylayers/measures/Hikobstart.py
@@ -45,7 +45,7 @@ def extract(D):
             trssi[k]=rssi[u[k]:]
     return(trssi)
 
-CORMORAN='/home/uguen/svn2/measures/CORMORAN/'
+CORMORAN='/media/niamiot/DONNEES/svn2/measures/CORMORAN'
 os.chdir(CORMORAN+'/RAW/11-06-2014/HIKOB')
 files = os.listdir('.')
 rawfiles = filter(lambda x: 'RAW' in x ,files)
@@ -158,7 +158,6 @@ def matstart(data,index):
                 tstart[k-1][l-1] = u.astype('int')
     return tstart
 
-tstart = matstart(data2,1).astype('int')
 
 def extracts(data2,tstart):
     datao ={}
@@ -177,16 +176,125 @@ def extracts(data2,tstart):
                 #del serie[k][l]['seq']
     return(serie,datao)
 
-tstart[8,6] = 3701
-tstart[6,8] = 3678
+
+def plotindex(dd):
+    f,ax=plt.subplots(nrows=16,ncols=16)
+    
+    for k in range(1,17):
+        for l in range(1,17):
+            if k!=l:
+                ax[k-1,l-1].plot(dd[k,l].index)
 
 print "get serie 2"
 
-s,data3 = extracts(data2,tstart)
+tstart = matstart(data2,1).astype('int')
+tstart[6,8] = 3678
+tstart[8,6] = 3701
 
+s,data3 = extracts(data2,tstart)
 S2 = pd.DataFrame(s)
 
+
+print "get serie 3"
+
 tstart3 = matstart(data3,1).astype('int')
+tstart3[6,8] = 3647
+tstart3[8,6] = 3663
+
+s,data4 = extracts(data3,tstart3)
+
+S3 = pd.DataFrame(s)
+
+print "get serie 4 --"
+
+tstart4 = matstart(data4,1).astype('int')
+tstart4[9,10] = 93
+tstart4[14,8] = 75
+
+s,data5 = extracts(data4,tstart4)
+
+S4 = pd.DataFrame(s)
+
+
+print "get serie 5 --"
+
+tstart5 = matstart(data5,1).astype('int')
+tstart5[9,10] = 48
+tstart5[14,8] = 22
+
+s,data6 = extracts(data5,tstart5)
+
+S5 = pd.DataFrame(s)
+
+print "get serie 6 --"
+
+tstart6 = matstart(data6,1).astype('int')
+tstart6[9,10] = 48
+tstart6[14,8] = 45
+tstart6[8,14] = 48
+
+s,data7 = extracts(data6,tstart6)
+
+S6 = pd.DataFrame(s)
+
+
+print "get serie 7 --"
+
+tstart7 = matstart(data7,1).astype('int')
+tstart7[9,10] = 37
+tstart7[14,8] = 27
+tstart7[8,14] = 35
+
+s,data8 = extracts(data7,tstart7)
+
+S7 = pd.DataFrame(s)
+##########
+print "get serie 8"
+
+tstart8 = matstart(data8,1).astype('int')
+s,data9 = extracts(data8,tstart8)
+
+S8 = pd.DataFrame(s)
+
+print "get serie 9"
+
+tstart9 = matstart(data9,1).astype('int')
+s,data10 = extracts(data9,tstart9)
+S9 = pd.DataFrame(s)
+
+print "get serie 10"
+
+tstart10 = matstart(data10,1).astype('int')
+#plt.plot(s[11,7].index)
+tstart10[8,6] = 4316
+tstart10[10,6] = 4400
+s,data11 = extracts(data10,tstart10)
+#plotindex(s)
+S10 = pd.DataFrame(s)
+
+
+print "get serie 11"
+
+tstart11 = matstart(data11,1).astype('int')
+tstart11[1,6] = 4332
+tstart11[8,6] = 4209
+tstart11[10,6] = 4289
+s,data12 = extracts(data11,tstart11)
+
+S11 = pd.DataFrame(s)
+
+
+# print "get serie 12"
+# # TO BE DONE
+# tstart12 = matstart(data12,1).astype('int')
+# tstart12[1,6] = 4332
+# tstart12[8,6] = 4209
+# tstart12[10,6] = 4289
+# s,data13 = extracts(data12,tstart12)
+
+# S12 = pd.DataFrame(s)
+
+
 #
 #
 #for sc in range(25):
