1864653bb72fc87d037840359f24bc229a6907bc
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2014-10-04 15:09:39 +0200

    Updating Website

--- modules/generated/pylayers-signal-bsignal-Bsignal-save-1.py
+++ modules/generated/pylayers-signal-bsignal-Bsignal-save-1.py
@@ -2,10 +2,10 @@ from pylayers.signal.bsignal import *
 import matplotlib.pyplot as plt
 e = EnImpulse(fe=100)
 fig,ax = e.plot(typ=['v'])
-plt.title('original waveform')
+tit1 = plt.title('original waveform')
 e.save('impulse.mat')
 del e
 h = TUsignal()
 h.load('impulse.mat')
 fig,ax = h.plot(typ=['v'])
-plt.title('retrieved waveform')
+tit2 = plt.title('retrieved waveform')
