bde98926211c331d542e3f0d795279ccaff62c27
Nicolas AMIOT <amiot.nicolas@gmail.com>
2014-10-29 10:29:29 +0100

    update project with new .pylayrs file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Setup script for pylayers
 import numpy
 
 from setuptools import setup,find_packages
-
+import os
 
 setup(name='pylayers' ,
       version='0.1',
@@ -20,16 +20,56 @@ setup(name='pylayers' ,
         'scipy>=0.10.1',
         'networkx>=1.7',
         'matplotlib>=1.1.0',
-        'shapely>=1.2.14',
         'basemap>=1.0',
         'SimPy==2.2',
         'PIL>=1.1.5',
         'bitstring>=3.0.2',
         'shapely>=1.2.14',
         'descartes>=1.0',
-        'osmapi>=0.3'
+        'osmapi>=0.3',
+        'imposm>=2.5'
                         ],
       packages=find_packages()
 )
 
+# detect if project and source are already locate in .pylayers
+# if not create both
+# if so just update the source path
 
+
+# detect user's home
+home = os.path.expanduser('~')
+# detect directory from which setup is launched
+source = os.getcwd()
+# project directory name
+project = 'pylayers_project'
+
+# check fresh install of pylayers
+if not os.path.isfile(os.path.join(home,'.pylayers')):
+    with open(os.path.join(home,'.pylayers'),'a') as f:
+        f.write('source :\n')
+        f.write(source)
+        basen_env = os.getenv('BASENAME')
+        # test if env var BASENAME already set 
+        #if not create a pyproject directory
+        if basen_env == None:
+            if not os.path.isdir(os.path.join(home,project)):
+                os.mkdir(os.path.join(home,project))
+        else:
+            project=basen_env
+        f.write('\nproject:\n')
+        f.write(os.path.join(home,project))
+
+# if pylayers has already been installer, a .pylayers exists. 
+# The idea here is to maintain the project path and ti update the source path.
+else: 
+    with open(os.path.join(home,'.pylayers'),'r') as f:
+        lines = f.readlines()
+        # line corresponding to the source's path
+    with open(os.path.join(home,'.pylayers'),'w') as f:
+        for il,l in enumerate(lines):
+            # line corresponding to the source's path
+            if il != 1:
+                f.write(l)
+            else :
+                f.write(source+"\n")
