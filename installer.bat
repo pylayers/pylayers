4f381696930f82b5f14575178ee0e53802789c78
Nicolas AMIOT <amiot.nicolas@gmail.com>
2014-11-04 16:56:29 +0100

    fix in installer.bat

--- installer.bat
+++ installer.bat
@@ -2,5 +2,5 @@ conda install mayavi --yes
 conda install basemap --yes
 conda install shapely --yes
 conda install gdal --yes
-pip install protobuf --yes
+pip install protobuf 
 python setup.py install
