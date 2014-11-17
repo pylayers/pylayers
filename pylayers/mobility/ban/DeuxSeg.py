fd2a5fd50f456e46781e940e57034571392f3661
Nicolas AMIOT <amiot.nicolas@gmail.com>
2014-11-17 10:24:12 +0100

    Full DeuxSeg MDA (between 3xNxtime and 3xMxtime)

--- pylayers/mobility/ban/DeuxSeg.py
+++ pylayers/mobility/ban/DeuxSeg.py
@@ -156,23 +156,31 @@ def dist(A,B,C,D,alpha,beta):
     """
      distance between AB-CD
 
-     AB (3xN)
-     CD (3xM)
+     AB (3xN) or (3xNxK) to add a time axis
+     CD (3xM) or (3xMxK) to add a time axis
+
 
      Parameters
      ----------
 
         A : (3xN) initial point segment 1
-        B :  (3xN) end point segment 1
-        C :  (3xM) starting point segment 2
-        D :  (3xM) end point segment 2  
-        alpha : (N x M) parametrization 
+            [or (3xNxK) initial point segment 1 for K realizations]
+        B   (3xN) end point segment 1
+            [or (3xNxK) end point segment 1 for K realizations]
+        C   (3xM) starting point segment 2
+            [or (3xMxK) initial point segment 2 for K realizations]
+        D   (3xM) end point segment 2  
+            [or (3xMxK) end point segment 2 for K realizations]
+        alpha : (N x M) parametrization
+            [or (NxMxK)] 
         beta : (N x M) parametrization 
-
+            [or (NxMxK)] 
     Returns
     -------
         f : N x M
+            [or (NxMxK)] 
         g : N x M
+            [or (NxMxK)] 
     """
     if len(A.shape) ==1 :
         A=A.reshape(3,1) 
@@ -198,17 +206,17 @@ def dist(A,B,C,D,alpha,beta):
     BA = A-B
 
     # u0 : N x M
-    u0 = np.einsum('ijk,ijk->jk',AC,AC)#np.dot(AC,AC)
+    u0 = np.einsum('ijk...,ijk...->jk...',AC,AC)#np.dot(AC,AC)
     # u4 : N 
-    u4 = np.einsum('ij,ij->j',BA,BA)[:,np.newaxis]#np.dot(BA,BA)
+    u4 = np.einsum('ij...,ij...->j...',BA,BA)[:,np.newaxis]#np.dot(BA,BA)
     # u5 : M 
-    u5 = np.einsum('ij,ij->j',CD,CD)[np.newaxis,:]#np.dot(CD,CD)
+    u5 = np.einsum('ij...,ij...->j...',CD,CD)[np.newaxis,:]#np.dot(CD,CD)
     # u1 : N x M
-    u1 = np.einsum('ij,ijk->jk',BA,AC)#np.dot(BA,AC)
+    u1 = np.einsum('ij...,ijk...->jk...',BA,AC)#np.dot(BA,AC)
     # u2 : N x M
-    u2 = np.einsum('ik,ijk->jk',CD,AC)#np.dot(CD,AC)
+    u2 = np.einsum('ik...,ijk...->jk...',CD,AC)#np.dot(CD,AC)
     # u3 : N x M
-    u3 = np.einsum('ik,ij->jk',CD,BA)#np.dot(CD,BA) 
+    u3 = np.einsum('ik...,ij...->jk...',CD,BA)#np.dot(CD,BA) 
 
 
     # f : N x M
@@ -218,7 +226,7 @@ def dist(A,B,C,D,alpha,beta):
     # Y : 3 x N x M
     Y  = C[:,np.newaxis,:] + beta[np.newaxis,:,:]*CD[:,np.newaxis,:]# C + beta*CD
     # g : N x M
-    g =np.einsum('ijk,ijk->jk',X-Y,X-Y)
+    g =np.einsum('ijk...,ijk...->jk...',X-Y,X-Y)
 
     return(f,g)
 
@@ -227,25 +235,32 @@ def dmin3d(A,B,C,D):
      dmin3d evaluate the minimal distance between 2 set of segments 
      Note that the number of segment of AB is NOT NECESSARILY the same than CD
 
-     AB (3xN)
-     CD (3xM)
+     AB (3xN) or (3xNxK) to add a time axis
+     CD (3xM) or (3xMxK) to add a time axis
 
      Parameters
      ----------
 
         A : (3xN) initial point segment 1
+            [or (3xNxK) initial point segment 1 for K realizations]
         B   (3xN) end point segment 1
+            [or (3xNxK) end point segment 1 for K realizations]
         C   (3xM) starting point segment 2
+            [or (3xMxK) initial point segment 2 for K realizations]
         D   (3xM) end point segment 2  
+            [or (3xMxK) end point segment 2 for K realizations]
 
     Returns
     -------
         alpha 
-            parametrization N x M
+            parametrization N x M 
+            [or (NxMxK) ]
         # beta 
-            parametrization N x M
+            parametrization N x M 
+            [or (NxMxK)]
         dmin 
-            minimal distance N x M
+            minimal distance N x M 
+            [or (NxMxK)]
     """
 
     if len(A.shape) ==1 :
@@ -266,17 +281,17 @@ def dmin3d(A,B,C,D):
     BA = A-B
 
     # u0 : N x M
-    u0 = np.einsum('ijk,ijk->jk',AC,AC)#np.dot(AC,AC)
+    u0 = np.einsum('ijk...,ijk...->jk...',AC,AC)#np.dot(AC,AC)
     # u4 : N 
-    u4 = np.einsum('ij,ij->j',BA,BA)[:,np.newaxis]#np.dot(BA,BA)
+    u4 = np.einsum('ij...,ij...->j...',BA,BA)[:,np.newaxis]#np.dot(BA,BA)
     # u5 : M 
-    u5 = np.einsum('ij,ij->j',CD,CD)[np.newaxis,:]#np.dot(CD,CD)
+    u5 = np.einsum('ij...,ij...->j...',CD,CD)[np.newaxis,:]#np.dot(CD,CD)
     # u1 : N x M
-    u1 = np.einsum('ij,ijk->jk',BA,AC)#np.dot(BA,AC)
+    u1 = np.einsum('ij...,ijk...->jk...',BA,AC)#np.dot(BA,AC)
     # u2 : N x M
-    u2 = np.einsum('ik,ijk->jk',CD,AC)#np.dot(CD,AC)
+    u2 = np.einsum('ik...,ijk...->jk...',CD,AC)#np.dot(CD,AC)
     # u3 : N x M
-    u3 = np.einsum('ik,ij->jk',CD,BA)#np.dot(CD,BA) 
+    u3 = np.einsum('ik...,ij...->jk...',CD,BA)#np.dot(CD,BA) 
 
     # den : N x M
     den   = u4*u5-u3*u3
@@ -289,6 +304,109 @@ def dmin3d(A,B,C,D):
 
     return(alpha,beta,dmin)
 
+
+def segdist(A,B,C,D,hard=True):
+    """
+     distance between AB-CD
+     Note that the number of segment of AB is NOT NECESSARILY the same than CD
+     (This function gathers dmin3d and dist from DeuxSeg)
+
+     AB (3xN) or (3xNxK) to add a time axis
+     CD (3xM) or (3xMxK) to add a time axis
+
+
+     Parameters
+     ----------
+
+        A : (3xN) initial point segment 1
+            [or (3xNxK) initial point segment 1 for K realizations]
+        B   (3xN) end point segment 1
+            [or (3xNxK) end point segment 1 for K realizations]
+        C   (3xM) starting point segment 2
+            [or (3xMxK) initial point segment 2 for K realizations]
+        D   (3xM) end point segment 2  
+            [or (3xMxK) end point segment 2 for K realizations]
+        hard : boolean:
+            if True when alpha<0 and beta<0 then alpha=0 and beta =0
+                    and when alpha>1 and beta>1 then alpha=1 and beta=1
+            elif False: use True alpha beta values in computation
+    Returns
+    -------
+
+        f : N x M
+            [or (NxMxK)] 
+        g : N x M
+            [or (NxMxK)] 
+        alpha : (N x M) parametrization
+            [or (NxMxK)] 
+        beta : (N x M) parametrization 
+            [or (NxMxK)] 
+        dmin 
+            minimal distance N x M 
+            [or (NxMxK)]
+    """
+
+    if len(A.shape) ==1 :
+        A=A.reshape(3,1) 
+    if len(B.shape) ==1 :
+        B=B.reshape(3,1) 
+    if len(C.shape) ==1 :
+        C=C.reshape(3,1) 
+    if len(D.shape) ==1 :
+        D=D.reshape(3,1) 
+
+
+    # 3 x N x M
+    AC = C[:,np.newaxis,:]-A[:,:,np.newaxis]
+    # 3 x M 
+    CD = D-C
+    # 3 x N 
+    BA = A-B
+
+    # u0 : N x M
+    u0 = np.einsum('ijk...,ijk...->jk...',AC,AC)#np.dot(AC,AC)
+    # u4 : N 
+    u4 = np.einsum('ij...,ij...->j...',BA,BA)[:,np.newaxis]#np.dot(BA,BA)
+    # u5 : M 
+    u5 = np.einsum('ij...,ij...->j...',CD,CD)[np.newaxis,:]#np.dot(CD,CD)
+    # u1 : N x M
+    u1 = np.einsum('ij...,ijk...->jk...',BA,AC)#np.dot(BA,AC)
+    # u2 : N x M
+    u2 = np.einsum('ik...,ijk...->jk...',CD,AC)#np.dot(CD,AC)
+    # u3 : N x M
+    u3 = np.einsum('ik...,ij...->jk...',CD,BA)#np.dot(CD,BA) 
+
+    # den : N x M
+    den   = u4*u5-u3*u3
+    # alpha = N x M
+    alpha = (u2*u3-u1*u5)/(1.*den)
+    # beta = N x M
+    beta  = (u1*u3-u2*u4)/(1.*den)
+    # dmin : N x M
+    dmin = np.sqrt(u0 + 2*(alpha*u1+beta*u2+alpha*beta*u3)+alpha*alpha*u4+ beta*beta*u5)
+
+    if hard :
+        ap = np.where(alpha>1)[0]
+        am = np.where(alpha<0)[0]
+        bp = np.where(beta>1)[0]
+        bm = np.where(beta<0)[0]
+        alpha[ap]=np.ones(len(ap))
+        alpha[am]=np.zeros(len(am))
+        beta[bp]=np.ones(len(bp))
+        beta[bm]=np.zeros(len(bm))
+
+
+
+    # f : N x M
+    f = u0 + 2*(alpha*u1+beta*u2+alpha*beta*u3)+alpha*alpha*u4+ beta*beta*u5
+    # X : 3 x N x M
+    X  = A[:,:,np.newaxis]-alpha[np.newaxis,:,:]*BA[:,:,np.newaxis] # A - alpha*BA
+    # Y : 3 x N x M
+    Y  = C[:,np.newaxis,:] + beta[np.newaxis,:,:]*CD[:,np.newaxis,:]# C + beta*CD
+    # g : N x M
+    g =np.einsum('ijk...,ijk...->jk...',X-Y,X-Y)
+
+    return(f,g,alpha,beta,dmin)
 
 
 # <codecell>
