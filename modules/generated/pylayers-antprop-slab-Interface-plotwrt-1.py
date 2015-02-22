425c58338fd0a31a483342447eb5c972c36be162
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2015-02-22 18:51:09 +0100

    Updating Website

---
+++ modules/generated/pylayers-antprop-slab-Interface-plotwrt-1.py
@@ -0,0 +1,17 @@
+from pylayers.antprop.slab import *
+import matplotlib.pylab as plt
+import numpy as np
+theta = np.arange(0,np.pi/2,0.01)
+fGHz = np.arange(0.1,10,0.2)
+sl = SlabDB('matDB.ini','slabDB.ini')
+mat = sl.mat
+lmatname = [mat['AIR'],mat['WOOD']]
+II = MatInterface(lmatname,0,fGHz,theta)
+II.RT()
+fig,ax = II.plotwrt(var='a',kv=10,typ=['m'])
+air = mat['AIR']
+brick = mat['BRICK']
+II = MatInterface([air,brick],0,fGHz,theta)
+II.RT()
+fig,ax = II.plotwrt(var='f',color='k',typ=['m'])
+plt.show()
