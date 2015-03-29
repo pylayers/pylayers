ef7385f749b2ee74bb1b796ffa2d5fd63bb67e89
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2015-03-29 18:27:25 +0200

    Updating Website

---
+++ modules/generated/pylayers-antprop-loss-Losst-1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from pylayers.simul.simulem import *
+from pylayers.measures.mesuwb import *
+from pylayers.antprop.loss import *
+S = Simul()
+S.layout('WHERE1.ini')
+fGHz = 4
+Tx,Rx = ptw1()
+Lwo,Lwp,Edo,Edp = Losst(S.L,fGHz,Tx.T,Rx[1,0:2],dB=True)
+fig=plt.figure(figsize=(20,10))
+fig,ax = S.L.showGs(fig=fig)
+tit = plt.title('test Losst')
+sc2 = ax.scatter(Rx[1,0],Rx[1,1],s=20,marker='x',c='k')
+sc1 = ax.scatter(Tx[:,0],Tx[:,1],s=20,c=Lwo,linewidth=0)
+cb = plt.colorbar(sc1)
+cb.set_label('dB')
+plt.show()
