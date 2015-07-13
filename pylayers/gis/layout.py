bcebfef4250557f5ec61c3e491fb9f31624b4d8d
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2015-07-13 12:32:07 +0200

    fixing in layout.py

--- pylayers/gis/layout.py
+++ pylayers/gis/layout.py
@@ -901,15 +901,16 @@ class Layout(PyLayers):
         #
         # Calculate the wedge angle of degree 2 points
         #
-        wedgea = self.wedge(self.degree[2])
+        if 2 in self.degree:
+            wedgea = self.wedge(self.degree[2])
 
         # wedge < 179 (not flat)
-        idiff = filter(lambda x: wedgea[x]<179,range(len(self.degree[2])))
-        self.ldiff = map(lambda x : self.degree[2][x],idiff)
+            idiff = filter(lambda x: wedgea[x]<179,range(len(self.degree[2])))
+            self.ldiff = map(lambda x : self.degree[2][x],idiff)
         # if problem here check file format 'z' should be a string
-        self.maxheight = np.max([v[1] for v in nx.get_node_attributes(self.Gs,'z').values()])
+            self.maxheight = np.max([v[1] for v in nx.get_node_attributes(self.Gs,'z').values()])
 
-        self.extrseg()
+            self.extrseg()
 
     def loadosm(self, _fileosm):
         """ load layout from an osm file format
