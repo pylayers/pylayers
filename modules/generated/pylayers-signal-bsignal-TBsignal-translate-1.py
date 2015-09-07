fd066e44cd6d736d9974955667330838d32bd9db
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2015-09-07 17:52:48 +0200

    Updating Website

--- modules/generated/pylayers-signal-bsignal-TBsignal-translate-1.py
+++ modules/generated/pylayers-signal-bsignal-TBsignal-translate-1.py
@@ -1,6 +1,7 @@
 from pylayers.signal.bsignal import *
 from matplotlib.pylab import *
-ip = EnImpulse()
+ip = TUsignal()
+ip.EnImpulse()
 ip.translate(-10)
 fig,ax=ip.plot(typ=['v'])
 show()
--- modules/generated/pylayers-signal-bsignal-Usignal-zlr-1.py
+++ modules/generated/pylayers-signal-bsignal-Usignal-zlr-1.py
@@ -1,6 +1,7 @@
 from pylayers.signal.bsignal import *
 from matplotlib.pylab import *
-ip = EnImpulse()
+ip = TUsignal()
+ip.EnImpulse()
 f,a = ip.plot(typ=['v'])
 ip.zlr(-10,10)
 f,a = ip.plot(typ=['v'])
