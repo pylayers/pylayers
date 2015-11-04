MAJOR FIX update : Fix broken Reflection coefficient computation

pylayers/antprop/interactions.py
@@ -188,8 +188,10 @@ class Inter(PyLayers):
             so : self.data[:,2]
 
         typ = 0
+            LOS
 
         typ = -1
+           Basis
 
         """
 
@@ -678,7 +680,6 @@ class IntR(Inter):
         data = np.array((ninter x [theta,si,st]))
 
         """
-
         self.sinsout()
 
         self.fGHz=fGHz
@@ -690,7 +691,8 @@ class IntR(Inter):
 
         if np.shape(self.data)[0]!=len(self.idx):
             self.data=self.data.T
-
+        import ipdb
+        ipdb.set_trace()
         if len(self.data) != 0:
             mapp = []
             # loop on all type of materials used for reflexion
@@ -711,6 +713,7 @@ class IntR(Inter):
             self.A[:, np.array((mapp)), :, :] = R
             self.alpha = np.array(self.alpha*len(self.idx), dtype=complex)
             self.gamma = np.array(self.gamma*len(self.idx), dtype=complex)
+
             return(self.A)
         else:
             self.A = self.data[:, None, None, None]

pylayers/antprop/slab.py
@@ -1300,7 +1300,7 @@ class MatDB(PyLayers,dict):
         fo.close()
 
 
-class Slab(dict,Interface):
+class Slab(Interface,dict):
     """ Handle a Slab
 
     Summary
@@ -1574,7 +1574,9 @@ class Slab(dict,Interface):
         name1 = '|'.join(mat['name'] for mat in self['lmat'])
         name2 = '|'.join(str(thick) for thick in self['lthick'])
         name = '(' + name1 + ')' + '(' + name2 + ')'
-        super(Slab,self).__init__(fGHz, theta, name=name)
+        import ipdb
+        ipdb.set_trace()
+        super(Slab,self).__init__(fGHz=fGHz, theta=theta, name=name)
         # Interface.__init__(self, fGHz, theta, name=name)
         #self.lmat = lmat
         #self.lthick = lthick
@@ -2542,10 +2544,7 @@ class Wedge(Interface,dict):
         self['N']=alpha/np.pi
 
 
-
-
-
-    # def phi0phin(self,u0,un,si,so):
+    # def phi0phin(self,u0,si,so):
     #     """
     #     Compute angle phi_0 and phi_nfrom face 0 regarding
     #     to unit vectors u0 and un along face 0 and n respectively
@@ -2555,14 +2554,13 @@ class Wedge(Interface,dict):
 
     #     u0 : ndarray (2|3xNp)
     #         unit vector along Np faces 0
-    #     un : ndarray (2|3xNp)
-    #         unit vector along Np faces n
     #     si : ndarray (2|3x)
     #         unit vector along incidence ray
     #     un : ndarray (2|3xNp)
     #         unit vector along Np faces n
-
     #     """
+
+    #     geu.
 
 
 def calsig(cval, fGHz, typ='epsr'):