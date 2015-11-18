Resolve issue in beta computation for diffraciton Fix #247

pylayers/antprop/rays.py
@@ -1650,7 +1650,18 @@ class Rays(PyLayers,dict):
                     phi = geu.vecang(vptpan,-sod[:2])
                     phi[~uleft] = geu.vecang(-sod[:2,~uleft],vptpan[:,~uleft])
                     # beta
-                    beta = geu.vecang(sid[[0,2]],vnormz[[0,2]])
+                    # it is important to check if the sid comes from left or right
+                    # to this end assume that sid vector is composed
+                    # of 2 point : (0,0) and sid
+                    # compared to the position of the diffraction point in x
+                    # with an elevation=0
+                    sidxz = sid[[0,2]]
+                    vnormxz = vnormz[[0,2]]
+                    zero = np.zeros((2,ptdiff.shape[1]))
+                    zdiff = np.vstack((ptdiff[0],zero[0]))
+                    left = geu.isleft(zero,sidxz,zdiff)
+
+                    beta = np.arccos(np.sum(vnormz*sid,axis=0))
 
                     # self[k]['diffvect'] is (4 x Nb_rays )
                     # for axis 0 lenght 4 represent :

pylayers/antprop/tests/test_reciprocity2.py
@@ -16,12 +16,12 @@ DL.wav = wvf.Waveform(fcGHz=5,bandGHz=3)
 # DL.eval(diffraction=True,force=True,ra_vectorized=True,alg=20152,si_reverb=2)
 DL.eval(diffraction=True,force=True,ra_vectorized=False,alg=5,cutoff=2,applywav=True)
 
-# DL2=DLink(L='defstr.ini')
+DL2=DLink(L='defstr.ini')
 
-# DL2.a=np.array([759,1114,1.0])
-# DL2.b=np.array([767,1114,1.5])
-# DL2.fGHz=np.arange(2,11,0.1)
-# DL2.wav = wvf.Waveform(fcGHz=5,bandGHz=3)
+DL2.a=np.array([759,1114,1.0])
+DL2.b=np.array([767,1114,1.5])
+DL2.fGHz=np.arange(2,11,0.1)
+DL2.wav = wvf.Waveform(fcGHz=5,bandGHz=3)
 # ##########################################################################
 # # !!never use the following part except for this particular test file !!!!
 # ##########################################################################