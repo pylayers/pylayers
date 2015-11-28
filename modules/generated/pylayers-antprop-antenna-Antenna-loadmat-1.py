e7fd899d0af202f46807523cc2918ad1d2cafb43
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2015-11-28 10:44:20 +0100

    Updating Website

--- modules/generated/pylayers-antprop-antenna-Antenna-loadmat-1.py
+++ modules/generated/pylayers-antprop-antenna-Antenna-loadmat-1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from pylayers.antprop.antenna import *
 A = Antenna('S1R1.mat',directory='ant/UWBAN/Matfile')
-f,a = A.plotG(phd=0)
-f,a = A.plotG(thd=90,fig=f,ax=a)
+f,a = A.plotG(plan='theta',angdeg=0)
+f,a = A.plotG(plan='phi',angdeg=90,fig=f,ax=a)
 txt = plt.title('S1R1 antenna : st loadmat')
 plt.show()
--- modules/generated/pylayers-antprop-antenna-Antenna-plotG-1.py
+++ modules/generated/pylayers-antprop-antenna-Antenna-plotG-1.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 from pylayers.antprop.antenna import *
 A = Antenna('defant.trx')
-fig,ax = A.plotG(fGHz=[2,3,4],phd=0)
-fig,ax = A.plotG(fGHz=[2,3,4],thd=90)
+fig,ax = A.plotG(fGHz=[2,3,4],plan='theta',angdeg=0)
+fig,ax = A.plotG(fGHz=[2,3,4],plan='phi',angdeg=90)
