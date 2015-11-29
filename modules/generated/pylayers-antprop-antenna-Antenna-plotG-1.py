ffeee30b10c6f40e7bbd9aac71cf0d1d1d6b2e48
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2015-11-29 16:17:32 +0100

    Updating Website

--- modules/generated/pylayers-antprop-antenna-Antenna-plotG-1.py
+++ modules/generated/pylayers-antprop-antenna-Antenna-plotG-1.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 from pylayers.antprop.antenna import *
-A = Antenna('defant.trx')
+A = Antenna('defant.vsh3')
 fig,ax = A.plotG(fGHz=[2,3,4],plan='theta',angdeg=0)
 fig,ax = A.plotG(fGHz=[2,3,4],plan='phi',angdeg=90)
