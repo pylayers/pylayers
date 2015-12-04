5684dc17f725c0234d2e099492c85071c1c4c81a
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2015-12-04 12:40:42 +0100

    add test_calibration

---
+++ pylayers/measures/test/test_calibration.py
@@ -0,0 +1,14 @@
+from pylayers.measures.vna.E5072A import *
+from pylayers.measures.parker.smparker import *
+from pylayers.antprop.channel import *
+
+vna = SCPI()
+vna.calibh5(_fileh5='test4Dec',
+         _filecal='cal_config.ini',
+         _filevna='vna_config.ini',
+         cables=['CN1','T','CN3'],
+         author='Mamadou',
+         comment='test')
+
+
+
