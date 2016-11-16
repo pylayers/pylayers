8baf24950a067d547101cf5ae4a7c8aae4ff0dee
Nicolas AMIOT <amiot.nicolas@gmail.com>
2016-11-16 15:43:56 +0100

    work on location.py

--- pylayers/location/observables.py
+++ pylayers/location/observables.py
@@ -4,7 +4,7 @@ import scipy as sp
 import pylayers.antprop.loss as plm
 import matplotlib.pyplot as plt
 
-class Observables():
+class Observables(object):
     """ Generate observables for localization prupose
     """
     def __init__(self, an=10 * sp.rand(3, 5), bn=5 * sp.rand(3,4)):
@@ -36,6 +36,13 @@ class Observables():
         self.noise_model : noise matrix ()
 
         """
+
+        if len(an.shape) > 2:
+            raise AttributeError('Anchors \'an\' shape must be (3 x Na)')
+
+        if len(bn.shape) > 2:
+            raise AttributeError('Blind nodes \'bn\' shape must be (3 x Nb)')
+
         if an.shape[0] == 2:
             an = np.vstack((an, np.zeros(an.shape[1])))
         elif an.shape[0] != 3:
@@ -47,6 +54,13 @@ class Observables():
         elif bn.shape[0] != 3:
             raise AttributeError('Blind nodes bn first dimension reserved to \
                                   space (x,y,z) coordinates')
+
+        if len(an.shape) == 1:
+            an = np.array([an])
+        if len(bn.shape) == 1:
+            bn = np.array([bn])
+
+
 
         self.an = an
         self.bn = bn
@@ -60,7 +74,36 @@ class Observables():
         self.compute_diff_ranges()
         self.compute_rpower()
 
-        self.create_noise()
+        self.config_noise()
+
+    @property
+    def rp_model(self):
+        return self._rp_model
+
+    @rp_model.setter
+    def rp_model(self, value):
+        if hasattr(self, 'rp_model'):
+            if self._rp_model != value:
+                self.compute_rpower(config=value)
+        else:
+            # first call from compute_power
+            self._rp_model = value
+
+    @property
+    def noise_model(self):
+        return self._noise_model
+
+    @noise_model.setter
+    def noise_model(self, value):
+        if hasattr(self, 'noise_model'):
+            idem = np.alltrue(np.array([i in value.items() for i in self.noise_model.items()]))
+            # check if any key/values of self.noise != new assigned value
+            if not idem:
+                self._noise_model = value
+                self.create_noise(config=value)
+        else:
+            # first call from compute_power
+            self._noise_model = value
 
 
     def __repr__(self):
@@ -77,15 +120,21 @@ class Observables():
         for b in self.bn:
             s = s + str(b) + "\n"
 
-        s = s + '\n\n' + 'self.Na : Number of anchor nodes (Na)'
+        s = s + '\n'
+        s = s + '\n' + 'self.Na : Number of anchor nodes (Na)'
         s = s + '\n' + 'self.Nb : Number of blind nodes  (Nb)'
+
         s = s + '\n' + 'self.dist : distances matrix (Nb x Na)'
         s = s + '\n' + 'self.rng : range matrix (Nb x Na)'
         s = s + '\n' + 'self.drng : difference of ranges matrix (Na x Nb x Na)'
+
         s = s + '\n' + 'self.rp : received power ( Nb x Na)'
         s = s + '\n' + 'self.rp_model : power model'
+
         s = s + '\n' + 'self.noise : noise matrix ()'
         s = s + '\n' + 'self.noise_model : noise matrix ()'
+
+        s = s + '\n' + 'self.generate_noise_samples() : update self.noise'
 
         return s
 
@@ -104,7 +153,8 @@ class Observables():
 
     def compute_ranges(self):
         """
-        Compute range in nanoseconds between all anchors an and all blind nodes bn
+        Compute range in nanoseconds between all anchors an
+        and all blind nodes bn
 
         Return
         ------
@@ -128,11 +178,11 @@ class Observables():
             difference of distances for each node as refernce:
             (Na x Nb x Na)
         """
-        ddist = np.ndarray(shape=(0,self.Nb,self.Na))
+        ddist = np.ndarray(shape=(0, self.Nb, self.Na))
         for a in xrange(self.Na):
-            diff = self.dist[:,a][:,None]-self.dist[:,:]
+            diff = self.dist[:, a][:, None] - self.dist[:, :]
 
-            ddist = np.vstack((ddist,diff[None,...]))
+            ddist = np.vstack((ddist, diff[None, ...]))
         self.ddist = ddist
 
     def compute_diff_ranges(self):
@@ -164,18 +214,33 @@ class Observables():
                 config['pl_exp'] : pathloss exponent
 
         """
-        if config== {}:
-            config['model']='PL'
-            config['d0']=1.
-            config['fGHz']=2.4
-            config['pl_exp']=2.
+
+        implemented_model=['PL']
+
+        if config == {}:
+            config['model'] = 'PL'
+            config['d0'] = 1.
+            config['fGHz'] = 2.4
+            config['pl_exp'] = 2.
+        else:
+            if isinstance(config, dict):
+                if config.has_key('model'):
+                    if config['model'] in implemented_model:
+                        pass
+                    else:
+                        raise AttributeError('model ' + str(config['model']) +
+                                              ' is not yet implemented')
+                else:
+                    raise AttributeError('config dict has no \'model\' key')
+            else:
+                raise AttributeError('config must be a dict instance')
 
         if config['model'] == 'PL':
             self.rp = -plm.PL0(config['fGHz'], config['d0']) +\
                           10 * config['pl_exp'] * np.log10(self.dist / config['d0'])
             self.rp_model = config
 
-    def create_noise(self,config={}) :
+    def config_noise(self, config={}):
         """
 
         Create a noise matrix for obserables
@@ -191,17 +256,40 @@ class Observables():
         -------
         """
 
+        implemented_law = ['norm']
+
         if config == {}:
             config['law'] = 'norm'
             config['mean'] = 0.
             config['std'] = 2.
+        else:
+            if isinstance(config, dict):
+                if config.has_key('law'):
+                    if config['law'] in implemented_law:
+                        pass
+                    else:
+                        raise AttributeError('law ' + str(config['law']) +
+                                              'is not yet implemented')
+                else:
+                    raise AttributeError('config dict has no \'law\' key')
+            else:
+                raise AttributeError('config must be a dict instance')
 
-        N = sp.stats.norm(loc=config['mean'],scale=config['std'])
-        noise = N.rvs((self.Nb,self.Na))
-        self.noise = noise
+        if config['law'] == 'norm':
+            self.law = sp.stats.norm(loc=config['mean'], scale=config['std'])
+
         self.noise_model = config
+        self.generate_noise_samples()
 
-    def show(self,**kwargs):
+    def generate_noise_samples(self):
+        """
+        Generate new noise samples relying on the law configuration 
+        setup in self.configure_noise
+        """
+
+        self.noise = self.law.rvs((self.Nb, self.Na))
+
+    def show(self, **kwargs):
         """
             Show scene
         """
@@ -228,6 +316,7 @@ class Observables():
         ax.scatter(self.bn[0,:],self.bn[1,:], c='r', marker='o', label='Blind nodes')
         plt.legend()
 
+        return fig, ax
 
 if (__name__ == "__main__"):
 
