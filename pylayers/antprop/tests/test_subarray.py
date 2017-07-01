4c50fa48178ed38088f8019e7d8fb55306aff64c
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-07-01 21:10:25 +0200

    None

---
+++ pylayers/antprop/tests/test_subarray.py
@@ -0,0 +1,14 @@
+from pylayers.antprop.aarray import *
+import matplotlib.pyplot as plt 
+print '--------------'
+print 'antprop/test_subarray.py'
+print '--------------'
+fcGHz = 60
+lamda = 0.3/fcGHz
+N1   = [ 4,4,1]
+N2   = [ 4,4,1]
+dm1 = [lamda/2.,lamda/2.,0]
+dm2 = [3*lamda,3*lamda.,0]
+A1 = AntArray(fGHz=np.array([fcGHz]),N=N1,dm=dm1,typant='Omni')
+A2 = AntArray(fGHz=np.array([fcGHz]),N=N2,dm=dm2,typant='Omni')
+A1.eval()
