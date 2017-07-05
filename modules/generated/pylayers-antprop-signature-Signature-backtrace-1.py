7726a894d21e31f321428148f2b2350aafb0a132
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-07-05 16:40:05 +0200

    Updating Website

--- modules/generated/pylayers-antprop-signature-Signature-backtrace-1.py
+++ modules/generated/pylayers-antprop-signature-Signature-backtrace-1.py
@@ -2,22 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pylayers.gis.layout import *
 from pylayers.antprop.signature import *
-L = Layout()
-L.dumpr()
-seq = np.array([[1,5,1],[1,1,1]])
+L = Layout('defstr.ini')
 s = Signature(seq)
-tx = np.array([4,-1])
-rx = np.array([1,1])
+tx = np.array([760,1113])
+rx = np.array([762,1114])
 s.ev(L)
 M = s.image(tx)
 isvalid,Y = s.backtrace(tx,rx,M)
-fig = plt.figure()
-ax = fig.add_subplot(111)
+
+fig,ax = L.showG('s',labels=1,aw=1,axes=1)
 l1 = ax.plot(tx[0],tx[1],'or')
 l2 = ax.plot(rx[0],rx[1],'og')
 l3 = ax.plot(M[0,:],M[1,:],'ob')
 l4 = ax.plot(Y[0,:],Y[1,:],'xk')
 ray = np.hstack((np.hstack((tx.reshape(2,1),Y)),rx.reshape(2,1)))
 l5 = ax.plot(ray[0,:],ray[1,:],color='#999999',alpha=0.6,linewidth=0.6)
-fig,ax = L.showG('s',fig=fig,ax=ax)
+
 plt.show()
