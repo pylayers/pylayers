541cd70a6147f5fc15c716cbc614fb87b9ab69e9
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-09-20 16:41:25 +0200

    Updating Website

--- _downloads/plot_exLayout2.py
+++ _downloads/plot_exLayout2.py
@@ -9,6 +9,7 @@ from pylayers.gis.layout import *
 # Load the layout from its .ini file in $BASENAME/struc/ini 
 L = Layout('WHERE1.lay')
 # Build all the graphs 
+L.build()
 # Check graphs
 L._visual_check()
 plt.show()
