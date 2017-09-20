fadf005f50a92e701b47136d91aae53629bcfe86
Nicolas AMIOT <amiot.nicolas@gmail.com>
2017-09-20 15:48:17 +0200

    Add Bthph to geom util, in order to pass th ph from a global basis to a local basis.

--- pylayers/util/tests/test_geomutil.py
+++ pylayers/util/tests/test_geomutil.py
@@ -72,46 +72,59 @@ class Tesgeu(TestCase):
         np.testing.assert_almost_equal(pg,[-1,0,0])  # polar along x
 
     def test_Bthph(self):
-        th = np.array([0.])
+        th = np.array([np.pi/2.])
         ph = np.array([np.pi/4.])
-        
         M=np.array([[0,-1,0],[1,0,0],[0,0,1]]) # rotation -np.pi/2 along z
-        thg,phg = Bthph(th,ph,M)
-        np.testing.assert_almost_equal(thg,0.)
-        np.testing.assert_almost_equal(phg,3*np.pi/4.)
-        
+        thg,phg = geu.Bthph(th,ph,M)
+        np.testing.assert_almost_equal(thg,np.pi/2.)
+        np.testing.assert_almost_equal(phg,-np.pi/4.)
 
-        th = np.array([0.])
+
+
+        th = np.array([np.pi/2.])
+
         ph = np.array([np.pi/4.])
 
         M=np.array([[1,0,0],[0,0,-1],[0,1,0]])# rotation -np.pi/2 along x
-        thg,phg = Bthph(th,ph,M)
-        np.testing.assert_almost_equal(thg,np.pi/4.)
+        thg,phg = geu.Bthph(th,ph,M)
+        np.testing.assert_almost_equal(thg,3*np.pi/4.)
         np.testing.assert_almost_equal(phg,0.)
-        
-        th = np.array([0.])
+
+
+        th = np.array([np.pi/2.])
         ph = np.array([np.pi/4.])
 
-        M=np.array([[1,0,0],[0,0,1],[0,-1,0]])# rotation -np.pi/2 along x
-        thg,phg = Bthph(th,ph,M)
-        np.testing.assert_almost_equal(thg,-np.pi/4.)
+        M=np.array([[1,0,0],[0,0,1],[0,-1,0]])# rotation np.pi/2 along x
+        thg,phg = geu.Bthph(th,ph,M)
+
+        np.testing.assert_almost_equal(thg,np.pi/4.)
         np.testing.assert_almost_equal(phg,0.)
+
+
 
         th = np.array([np.pi/4])
         ph = np.array([0.])
 
         M=np.array([[1,0,0],[0,0,1],[0,-1,0]]) # rotation np.pi/2 along x
-        thg,phg = Bthph(th,ph,M)
-        np.testing.assert_almost_equal(thg,0.)
+        thg,phg = geu.Bthph(th,ph,M)
+        np.testing.assert_almost_equal(thg,np.pi/2.)
         np.testing.assert_almost_equal(phg,-np.pi/4.)
-        
-        th = np.array([np.pi/4])
-        ph = np.array([0.])
+
+
+
 
         M=np.array([[1,0,0],[0,0,-1],[0,1,0]]) # rotation -np.pi/2 along x
-        thg,phg = Bthph(th,ph,M)
-        np.testing.assert_almost_equal(thg,0.)
+        thg,phg = geu.Bthph(th,ph,M)
+        np.testing.assert_almost_equal(thg,np.pi/2.)
         np.testing.assert_almost_equal(phg,np.pi/4.)
+
+
+        M=np.eye(3) 
+        thg,phg = geu.Bthph(th,ph,M)
+
+
+        np.testing.assert_almost_equal(thg,th)
+        np.testing.assert_almost_equal(phg,ph)
 
 
     def test_intersect3(self):
