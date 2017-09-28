c2f271d5b6550dc2b7c6cea1da21698e430e6923
dialounke <mamadou.balde@univ-rennes1.fr>
2017-09-28 14:48:42 +0200

    ras

--- pylayers/util/geomutil.py
+++ pylayers/util/geomutil.py
@@ -3891,7 +3891,7 @@ def SphericalBasis(a):
                     np.sin(a[:, 0]) * np.sin(a[:, 1]), 
                     np.cos(a[:, 0]))).T
     M = np.dstack((tha, pha, sa)).T
-    # M = np.swapaxes(M,0,1)
+    M = np.swapaxes(M,0,1)
     return M
 
 
