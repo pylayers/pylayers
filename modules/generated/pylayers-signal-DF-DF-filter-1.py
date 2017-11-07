6850536448c4d91cf3e360c5ef170c5bf0f2e7c6
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-11-07 18:13:11 +0100

    Updating Website

---
+++ modules/generated/pylayers-signal-DF-DF-filter-1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+df = DF(b=np.array([1,1],a=np.array([1,-1]))
+N = 100 
+s = np.zeros(N)
+s[0] = 1
+y = df.filter(s)
+plt.stem(np.arange(N),x)
+plt.stem(np.arange(N),y)
