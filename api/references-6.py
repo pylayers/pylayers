347e8e3349ad84f96861eb65d7890edd6a3cc273
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-11-08 18:42:31 +0100

    Updating Website

--- api/references-6.py
+++ api/references-6.py
@@ -1,21 +1,12 @@
+from pylayers.signal.bsignal import *
 import matplotlib.pyplot as plt
-import numpy as np
-from pylayers.gis.layout import *
-from pylayers.antprop.signature import *
-L = Layout('defstr.ini')
-s = Signature(seq)
-tx = np.array([760,1113])
-rx = np.array([762,1114])
-s.ev(L)
-M = s.image(tx)
-isvalid,Y = s.backtrace(tx,rx,M)
-
-fig,ax = L.showG('s',labels=1,aw=1,axes=1)
-l1 = ax.plot(tx[0],tx[1],'or')
-l2 = ax.plot(rx[0],rx[1],'og')
-l3 = ax.plot(M[0,:],M[1,:],'ob')
-l4 = ax.plot(Y[0,:],Y[1,:],'xk')
-ray = np.hstack((np.hstack((tx.reshape(2,1),Y)),rx.reshape(2,1)))
-l5 = ax.plot(ray[0,:],ray[1,:],color='#999999',alpha=0.6,linewidth=0.6)
-
-plt.show()
+x = np.array( [ 1, 3 , 6 , 11 , 18])
+y = np.array( [ 0,1 ,-5, 8 , 10])
+sb = TBsignal(x,y)
+su20 = sb.b2tu(20)
+su100 = sb.b2tu(100)
+fi = plt.figure()
+st = sb.stem()
+fig,ax = su20.plot(color='k')
+fig,ax = su100.plot(color='r')
+ti = plt.title('b2tu : sb(blue) su20(black) su200(red)')
