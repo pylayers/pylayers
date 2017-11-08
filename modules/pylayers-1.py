daebdd129423eb963f25ed3d27bbb8a06dd27a2d
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-11-08 10:59:59 +0100

    Updating Website

--- modules/pylayers-1.py
+++ modules/pylayers-1.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
-from pylayers.antprop.antenna import *
-A = Antenna()
-fig,ax = A.plotG(fGHz=[2,3,4],plan='theta',angdeg=0)
+from pylayers.gis.layout import *
+L = Layout('WHERE1.ini')
+L.loadfur('Furw1.ini')
+fig = plt.figure()
+ax = fig.gca()
+fig,ax = L.showGs(fig=fig,ax=ax,furniture=True)
+ti = plt.title('loadfur')
+plt.show()
--- modules/pylayers-4.py
+++ modules/pylayers-4.py
@@ -1,5 +1,4 @@
+import matplotlib.pyplot as plt
 from pylayers.antprop.antenna import *
-A = Antenna('S2R2.sh3')
-A.eval()
-tau = A.getdelay()
-A.elec_delay(tau)
+A = Antenna()
+fig,ax = A.plotG(fGHz=[2,3,4],plan='theta',angdeg=0)
