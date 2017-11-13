9e14a2dd89b87f862e7a6019aaac79a3d7eeea86
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2017-11-13 12:44:59 +0100

    fix in importosm

--- pylayers/gis/layout.py
+++ pylayers/gis/layout.py
@@ -1030,6 +1030,7 @@ class Layout(pro.PyLayers):
         # matrix segment-segment 
         # usage 
         # self.sgsg[seg1,seg2] => return common point
+
         mno = max(self.Gs.nodes())
         self.sgsg = sparse.lil_matrix((mno+1,mno+1),dtype='int')
 
