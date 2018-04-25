44ef292c518eba78fc6e251adcca133179758aa5
Bernard Uguen <bernard.uguen@univ-rennes1.fr>
2018-04-25 15:14:33 +0200

    None

--- setup.py
+++ setup.py
@@ -49,11 +49,7 @@ if not os.path.isfile(os.path.join(home,'.pylayers')):
         f.write('\nproject\n')
         f.write(os.path.join(home,project))
 
-<<<<<<< HEAD
-# if pylayers has already been installer, a .pylayers exists.
-=======
-# if pylayers has already been installed, a .pylayers exists. 
->>>>>>> master
+# if pylayers has already been installed, a .pylayers exists.
 # The idea here is to maintain the project path and to update the source path.
 else:
     with open(os.path.join(home,'.pylayers'),'r') as f:
