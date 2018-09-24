b2c8f7c952477528ac8bfaf6c41e29595c0525f1
Uguen <bernard.uguen@univ-rennes1.fr>
2018-09-24 16:30:28 +0200

    Updating Website

--- api/pylayers-gis-layout-Layout-2.py
+++ api/pylayers-gis-layout-Layout-2.py
@@ -3,5 +3,5 @@ L = Layout('TA-Office.lay')
 p1 = np.array([[0,0,0],[0,0,0]])
 p2 = np.array([[10,10,10],[10,10,10]])
 seglist = L.seginframe2(p1,p2)
-edlist  = [ L.tsg[x] for x in  seglist)
+edlist  = [ L.tsg[x] for x in  seglist ]
 fig,ax = L.showG('s',edlist=edlist)
